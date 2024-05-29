1cd8407e457bc4e379e9cbf401bc694a49eeb47f
Gregory Smelkov <gsmelkov@gmail.com>
2024-05-29 15:20:25 -0400

    Undo onclick

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -29,10 +29,6 @@ export default function Contact() {
         <a
           href="/#"
           className="block text-green-700 opacity-25 transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-100"
-          onClick={(e) => {
-            e.preventDefault();
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }}
         >
           <GiWarpPipe className="h-20 w-20" />
         </a>
