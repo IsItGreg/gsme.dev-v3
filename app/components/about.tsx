2d2d37109c422c2de396b684c79367cae6e0e37c
Gregory Smelkov <gsmelkov@gmail.com>
2024-10-17 15:59:47 -0400

    Fix desc

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,8 +4,8 @@ export default function About() {
       <div className="mx-auto flex max-w-lg flex-col">
         <h1 className="text-2xl sm:text-3xl">Hi, I'm Greg 👋</h1>
         <p className="mt-8 text-lg sm:text-xl">
-          I'm a frontend leaning full-stack software engineer in Boston. I enjoy
-          solving problems with code.
+          I'm a frontend leaning full-stack software engineer based in Boston. I
+          enjoy solving problems with code.
         </p>
       </div>
     </div>
